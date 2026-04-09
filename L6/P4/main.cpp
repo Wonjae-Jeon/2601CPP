b33b0a610fab0c010fd707f343d4c0b1f7776607
Wonjae-Jeon <axd1109@gmail.com>
2026-04-09 10:50:01 +0900

    Add files via upload

---
+++ L6/P4/main.cpp
@@ -0,0 +1,18 @@
+/*
+	Author: 전원재
+	Student Number: 2023041005
+	Date: 20260409
+	Class: C++
+	Lecture: 6
+	Problem: 4
+*/
+
+#include "Calculator.h"
+
+int main() {
+
+	Calculator calc; // calc 객체 생성
+	calc.run(); // 계산기 시작
+
+	return 0;
+}
