fe5ba6dee601022a5503296e613f6de7e7635aba
Wonjae-Jeon <axd1109@gmail.com>
2026-05-14 11:03:11 +0900

    Add files via upload

---
+++ L10/P1/main.cpp
@@ -0,0 +1,14 @@
+#include "Book.cpp"
+
+int main() {
+
+	Book book;
+
+	Book a("청춘", 20000, 300), b("미래", 30000, 500);
+	a += 500; // 책 a의 가격 500원 증가
+	b -= 500; // 책 b의 가격 500원 감소
+	a.show();
+	b.show();
+
+	return 0;
+}
