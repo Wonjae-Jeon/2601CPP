cf5a7dec00f7c8c0774c9264568c918a8a4ca292
Wonjae-Jeon <axd1109@gmail.com>
2026-05-28 10:49:19 +0900

    Add files via upload

---
+++ L12/P3/Calculator.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Calculator {
+	void input() {
+		cout << "정수 2 개를 입력하세요>> ";
+		cin >> a >> b;
+	}
+protected:
+	int a, b;
+	virtual int calc(int a, int b) = 0; // 두 정수의 합 리턴
+public:
+	void run() {
+		input();
+		cout << "계산된 값은 " << calc(a, b) << endl;
+	}
+};
+
+class Adder : public Calculator {
+	int calc(int a, int b) {
+		return a + b;
+	}
+};
+
+class Subtractor : public Calculator {
+	int calc(int a, int b) {
+		return a - b;
+	}
+};
---
+++ L12/P3/main.cpp
@@ -0,0 +1,8 @@
+#include "Calculator.cpp"
+
+int main() {
+	Adder adder;
+	Subtractor subtractor;
+	adder.run();
+	subtractor.run();
+}
